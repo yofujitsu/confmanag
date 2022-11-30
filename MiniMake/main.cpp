12b8c1cbc2cfce555d4747e0d252b20bedf81151
富士 <78373273+yofujitsu@users.noreply.github.com>
2022-11-30 17:14:28 +0300

    4 дз
    4 дз

---
+++ MiniMake/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main(){
+    print_hello();
+    cout << endl;
+    cout << "Factorial of 10 = " << factorial(10) << endl;
+    return 0;
+}
